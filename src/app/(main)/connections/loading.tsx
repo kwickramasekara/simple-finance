2e60bb414f045cc587a1acaab00428d2f901d8eb
Keith Wickramasekara <6528584+kwickramasekara@users.noreply.github.com>
2024-12-12 19:41:01 -0600

    Create page loaders

---
+++ src/app/(main)/connections/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from "@/components/main/loading";
+
+export default function ConnectionsPageLoader() {
+  return (
+    <main className="pt-20">
+      <Loading />
+    </main>
+  );
+}
